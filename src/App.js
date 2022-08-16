9b5bed1e45716c652776a18df77fb038883e2744
Surya Patil <patil.surya01@gmail.com>
2022-08-15 21:08:15 -0400

    m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Popup  from './Popup.js';
+import axios from 'axios';
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [file, setFile] = useState();
+  const [filename, setFilename] = useState('Choose File'); 
+
+  function handleChange(event){
+    setFile(event.target.files[0])
+    setFilename(event.target.files[0].name); 
+
+    console.log(filename); 
+  }
+
+  function handleSubmit(event){
+
+    event.preventDefault()
+    const formData  = new FormData(); 
+    formData.append('file',file);
+    const url = '/Users/suryapatil/hacktherunway/client/public/uploads'
+    const config = {
+      headers: {
+        'Content-Type':'multipart/form-data',
+      },
+    };
+    axios.post(url, formData, config).then((response) => {
+      console.log(response.data); 
+    })
+
+  }
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const renderGrid = () => {
+
+    
+  }
+
+  const FileUploadPage = () => {
+
+    
+    return (
+      <div className='fileUploadPage'>
+        <form onSubmit={handleSubmit}>
+          <input type = "file" onChange={handleChange}/>
+          <button type = "submit">Upload</button>
+        </form>
+      </div>
+    );
+    
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <h1 className = "Clips">
+        Clips
+        <button className="AddClip" onClick = {() => {togglePopup()}}>
+          Add Clip
+        </button>
+        {isOpen && <Popup
+      content={<>
+        {FileUploadPage()}
+      </>}
+      handleClose={togglePopup}
+    />}
+ 
+      </h1>
+      <div>
+        {renderGrid()}
+      </div>
+
+      <h2 className = "Participants">
+        Particpants
+        <button className="AddParts">
+          Add Participants
+        </button>
+      </h2>
+
+      <button className = "Present">
+        Present
+      </button>
+
     </div>
+    
   );
 }
 
